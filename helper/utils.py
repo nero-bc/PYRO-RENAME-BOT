b16e07c153a0dfbd92041bf18e30bbeb43ae4114
Dhanu990 <130265533+Dhanu990@users.noreply.github.com>
2023-10-26 21:25:09 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -1,18 +1,14 @@
-import math
-import time
+import math, time
 from datetime import datetime
 from pytz import timezone
-from config import Config, Txt
-from pyrogram import Client
+from config import Config, Txt 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-# Initialize your Pyrogram Client
-app = Client("my_account")
 
-async def progress_for_pyrogram(current, total, ud_type, message, start, additional_info, another_detail, file_name, date):
+async def progress_for_pyrogram(current, total, ud_type, message, start):
     now = time.time()
     diff = now - start
-    if round(diff % 5.00) == 0 or current == total:
+    if round(diff % 5.00) == 0 or current == total:        
         percentage = current * 100 / total
         speed = current / diff
         elapsed_time = round(diff) * 1000
@@ -22,63 +18,55 @@ async def progress_for_pyrogram(current, total, ud_type, message, start, additio
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "⬢" * math.floor(percentage / 5) + "⬡" * (20 - math.floor(percentage / 5))
-        tmp = PROGRESS_BAR.format(
+        progress = "{0}{1}".format(
+            ''.join(["⬢" for i in range(math.floor(percentage / 5))]),
+            ''.join(["⬡" for i in range(20 - math.floor(percentage / 5))])
+        )            
+        tmp = progress + Txt.PROGRESS_BAR.format( 
             round(percentage, 2),
             humanbytes(current),
             humanbytes(total),
-            humanbytes(speed),
-            estimated_total_time if estimated_total_time != '' else "0 s",
-            additional_info,
-            another_detail,
-            file_name,
-            date
+            humanbytes(speed),            
+            estimated_total_time if estimated_total_time != '' else "0 s"
         )
         try:
             await message.edit(
-                text=f"{ud_type}\n\n{tmp}",
-                reply_markup=InlineKeyboardMarkup([[
-                    InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️", callback_data="close")
-                ]])
+                text=f"{ud_type}\n\n{tmp}",               
+                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️", callback_data="close")]])                                               
             )
         except:
             pass
 
-def humanbytes(size):
+def humanbytes(size):    
     if not size:
         return ""
-    power = 2 ** 10
+    power = 2**10
     n = 0
     Dic_powerN = {0: ' ', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
     while size > power:
         size /= power
         n += 1
-    return f"{round(size, 2)} {Dic_powerN[n]}ʙ"
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'ʙ'
+
 
 def TimeFormatter(milliseconds: int) -> str:
     seconds, milliseconds = divmod(int(milliseconds), 1000)
     minutes, seconds = divmod(seconds, 60)
     hours, minutes = divmod(minutes, 60)
     days, hours = divmod(hours, 24)
-    tmp = (
-        (f"{days}ᴅ, ") if days else ""
-    ) + (
-        (f"{hours}ʜ, ") if hours else ""
-    ) + (
-        (f"{minutes}ᴍ, ") if minutes else ""
-    ) + (
-        (f"{seconds}ꜱ, ") if seconds else ""
-    ) + (
-        (f"{milliseconds}ᴍꜱ, ") if milliseconds else ""
-    )
-    return tmp[:-2]
+    tmp = ((str(days) + "ᴅ, ") if days else "") + \
+        ((str(hours) + "ʜ, ") if hours else "") + \
+        ((str(minutes) + "ᴍ, ") if minutes else "") + \
+        ((str(seconds) + "ꜱ, ") if seconds else "") + \
+        ((str(milliseconds) + "ᴍꜱ, ") if milliseconds else "")
+    return tmp[:-2] 
 
 def convert(seconds):
     seconds = seconds % (24 * 3600)
     hour = seconds // 3600
     seconds %= 3600
     minutes = seconds // 60
-    seconds %= 60
+    seconds %= 60      
     return "%d:%02d:%02d" % (hour, minutes, seconds)
 
 async def send_log(b, u):
@@ -90,6 +78,7 @@ async def send_log(b, u):
             Config.LOG_CHANNEL,
             f"**--Nᴇᴡ Uꜱᴇʀ Sᴛᴀʀᴛᴇᴅ Tʜᴇ Bᴏᴛ--**\n\nUꜱᴇʀ: {u.mention}\nIᴅ: `{u.id}`\nUɴ: @{u.username}\n\nDᴀᴛᴇ: {date}\nTɪᴍᴇ: {time}\n\nBy: {b.mention}"
         )
+        
 
-# Start your Pyrogram Client
-app.run()
+
+
